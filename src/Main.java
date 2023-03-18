c028d124c8229573a12b4eae865c82ac183b0f50
Jackson Almeida <jacksonnalmeida30@gmail.com>
2023-03-18 11:51:06 -0300

    format localdate e localdatetime

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,18 @@ import java.text.SimpleDateFormat;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class Main {
     public static void main(String[] args) {
+        DateTimeFormatter df = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        DateTimeFormatter df1 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
         LocalDate dt = LocalDate.now();
         LocalDateTime dt1 = LocalDateTime.now();
         Instant st = Instant.now();
 
-        SimpleDateFormat df = new SimpleDateFormat();
-
-        LocalDate pp = LocalDate.parse("2023-03-18");
+        LocalDate pp = LocalDate.parse("18/08/2023", df);
+        LocalDateTime pt = LocalDateTime.parse("18/08/2023 18:01", df1);
 
         System.out.println("Data local: "+ dt);
         System.out.println("Data e hora local: "+ dt1);
@@ -19,5 +21,6 @@ public class Main {
 
         System.out.println("_________________________________________");
         System.out.println("Parse data: "+ pp);
+        System.out.println("Parse data e hora: "+ pt);
     }
 }
